ea27e4669ce7158780576dec2d33a1661d5f7b04
Kang MinSeok <alstjr8125@naver.com>
2024-07-21 14:31:18 +0900

    Header, Home

---
+++ src/api.ts
@@ -0,0 +1,36 @@
+const API_KEY = "dbd7cf8251c348cadfefc4f7d3b6f189";
+const BASE_PATH = "https://api.themoviedb.org/3";
+
+interface IMovie {
+  //   adult: boolean;
+  backdrop_path: string;
+  //   genre_ids: number[];
+  id: number;
+  //   original_language: string;
+  //   original_title: string;
+  overview: string;
+  //   popularity: number;
+  poster_path: string;
+  //   release_date: string;
+  title: string;
+  //   video: boolean;
+  //   vote_average: number;
+  //   vote_count: number;
+}
+
+export interface IGetMoviesResult {
+  dates: {
+    maximum: string;
+    minimum: string;
+  };
+  page: number;
+  results: IMovie[];
+  total_pages: number;
+  total_results: number;
+}
+
+export function getMovies() {
+  return fetch(`${BASE_PATH}/movie/now_playing?api_key=${API_KEY}`).then(
+    (response) => response.json()
+  );
+}
